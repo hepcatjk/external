dd802d5d55541c394c2ee2956d3ee9a994897d80
Joe Kider <kiderj@gmail.com>
2024-12-01 19:43:47 -0500

    library updates

---
+++ submoduleadd.sh
@@ -0,0 +1,30 @@
+
+git submodule add https://github.com/madler/zlib external_libraries/zlib
+git submodule add https://github.com/tukaani-project/xz external_libraries/xz
+git submodule add https://github.com/rogersce/cnpy external_libraries/cnpy
+git submodule add https://github.com/google/glog external_libraries/glog
+git submodule add https://github.com/google/googletest external_libraries/googletest
+
+
+
+git submodule add https://github.com/pnggroup/libpng external_libraries/libpng
+git submodule add https://github.com/AcademySoftwareFoundation/Imath external_libraries/Imath
+git submodule add https://github.com/AcademySoftwareFoundation/openexr external_libraries/openexr
+
+#git submodule add https://github.com/01org/tbb external_libraries/tbb
+#git submodule add https://github.com/wjakob/nanogui external_libraries/nanogui
+#git submodule add https://github.com/embree/embree external_libraries/embree
+#git submodule add https://github.com/syoyo/tinyobjloader external_libraries/tinyobjloader
+
+
+git submodule add https://github.com/fastfloat/fast_float external_libraries/fast_float
+git submodule add https://github.com/g-truc/glm external_libraries/glm
+
+#header only
+git submodule add https://gitlab.com/libeigen/eigen external_libraries/eigen
+git submodule add https://github.com/hepcatjk/optionparser external_libraries/optionparser
+git submodule add https://github.com/c42f/tinyformat external_libraries/tinyformat
+git submodule add https://github.com/Tencent/rapidjson external_libraries/rapidjson
+git submodule add https://github.com/nothings/stb external_libraries/stb
+
+
